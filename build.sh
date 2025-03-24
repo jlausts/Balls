4835cc1f32288ee5ebc418a0a7e3f809cf86a2a6
jlausts <52058056+jlausts@users.noreply.github.com>
2025-03-24 09:09:26 -0600

    made build file

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+CC=gcc
+CFLAGS="-Wall -Werror -Wpedantic -Wextra -Wunused-variable -Wuninitialized -Wshadow -Wformat -Wconversion -Wfloat-equal -Wcast-qual -Wcast-align -Wstrict-aliasing -Wswitch-default -Werror=return-type -Werror=uninitialized -Werror=sign-compare -Wunused-function -Werror=aggressive-loop-optimizations -Werror=array-bounds -Ofast -funroll-loops -finline-functions -march=native -fpeel-loops "
+LIBS="-lm -lX11 -fopenmp"
+SRC="main.c"
+OUT="main"
+
+echo "Compiling $SRC..."
+$CC $CFLAGS -o $OUT $SRC $LIBS
+
+if [ $? -eq 0 ]; then
+    echo "Build successful."
+    ./$OUT
+else
+    echo "Build failed."
+fi
+
